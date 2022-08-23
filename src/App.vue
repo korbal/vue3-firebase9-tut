b19ec339aa82fb42f97d7849bfd4c366640e237a
korbal <balint.j.korosi@gmail.com>
2022-08-23 16:41:06 +0200

    added add todo

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,33 @@
 <template>
-  Badass todo
+
+  <div class="badass-todo">
+    <div class="title has-text-centered">Badass Todo</div>
+    <div class="field is-grouped">
+  <p class="control is-expanded">
+    <input class="input" type="text" placeholder="Add a todo">
+  </p>
+  <p class="control">
+    <a class="button is-info">
+      Add
+    </a>
+  </p>
+</div>
+
+
+  </div>
+  
+
 </template>
+
+<style>
+
+@import 'bulma/css/bulma.min.css';
+
+.badass-todo {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+
+
+</style>
